b60e5bc561c93a96ce8a09c96dbad950fb49f738
Negar Baharmand <negarbaharmand@outlook.com>
2023-03-22 19:22:48 +0100

    Task list display

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,9 +5,11 @@ import styles from "./Tasklist.module.css";
 const TaskList = ({ tasks }) => {
   return (
     <ul className={StyleSheet.tasks}>
-      {tasks.map((task) => {
-        <TaskItem key={task.id} task={task} />;
-      })}
+      {tasks
+        .sort((a, b) => b.id - a.id)
+        .map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
     </ul>
   );
 };
