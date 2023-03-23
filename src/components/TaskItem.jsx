b6b547bd6bb0beb52bf8d959df2e1c1545494c1a
Negar Baharmand <negarbaharmand@outlook.com>
2023-03-23 10:24:23 +0100

    Add functionality to delete button

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -6,7 +6,7 @@ import { CheckIcon } from "@heroicons/react/24/outline";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { TrashIcon } from "@heroicons/react/24/outline";
 
-export const TaskItem = ({ task }) => {
+const TaskItem = ({ task, deleteTask }) => {
   const [isChecked, setIsChecked] = useState(task.checked);
 
   const handleCheckboxChange = (e) => {
